f5be6015c5c3daef6093fe7a0394d1e85e8c9936
SpiceyWolf <spiceywolf.archaicsoft@outlook.com>
2020-08-26 13:09:02 -0400

    Moves the last 2 changes (Was happening too early causing crash)

--- src/BkCharacter/BkCharacter.ts
+++ src/BkCharacter/BkCharacter.ts
@@ -1,5 +1,5 @@
-import { IModLoaderAPI, IPlugin, IPluginServerConfig } from 'modloader64_api/IModLoaderAPI';
 import { InjectCore } from 'modloader64_api/CoreInjection';
+import { IModLoaderAPI, IPlugin } from 'modloader64_api/IModLoaderAPI';
 import { TunnelMessageHandler } from 'modloader64_api/GUITunnel';
 import * as API from 'BanjoKazooie/API/Imports';
 
@@ -23,36 +23,38 @@ export class BkCharacter implements IPlugin {
 
     init(): void {
         let config = this.ModLoader.config.registerConfigCategory('BkCharacter') as IConfig;
-        this.ModLoader.config.setData('BkCharacter', 'bear_bird', 'BANJO_KAZOOIE'); 
+        this.ModLoader.config.setData('BkCharacter', 'bear_bird', 'BANJO_KAZOOIE');
         this.ModLoader.config.setData('BkCharacter', 'termite', 'BANJO_TERMITE');
         this.ModLoader.config.setData('BkCharacter', 'crocodile', 'BANJO_CROCODILE');
         this.ModLoader.config.setData('BkCharacter', 'walrus', 'BANJO_WALRUS');
         this.ModLoader.config.setData('BkCharacter', 'pumpkin', 'BANJO_PUMPKIN');
         this.ModLoader.config.setData('BkCharacter', 'bee', 'BANJO_BEE');
         this.ModLoader.config.save();
+    }
 
+    postinit(): void {
         this.core.character.active = true;
+        let config = this.ModLoader.config.registerConfigCategory('BkCharacter') as IConfig;
         this.core.character.bear_bird_id = (<any>API.CharacterType)[config.bear_bird];
         this.core.character.termite_id = (<any>API.CharacterType)[config.termite];
         this.core.character.crocodile_id = (<any>API.CharacterType)[config.crocodile];
         this.core.character.walrus_id = (<any>API.CharacterType)[config.walrus];
         this.core.character.pumpkin_id = (<any>API.CharacterType)[config.pumpkin];
         this.core.character.bee_id = (<any>API.CharacterType)[config.bee];
+        this.ModLoader.gui.openWindow(330, 190, __dirname + '/Character.html');
     }
-
-    postinit(): void { this.ModLoader.gui.openWindow(330, 190, __dirname + '/Character.html'); }
 
     onTick(): void { }
 
     @TunnelMessageHandler('BkCharacter:request')
     onCharacterRequest(evt: any) {
         let config = this.ModLoader.config.registerConfigCategory('BkCharacter') as IConfig;
-        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'bearbird', value: config.bear_bird});
-        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'termite', value: config.termite});
-        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'crocodile', value: config.crocodile});
-        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'walrus', value: config.walrus});
-        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'pumpkin', value: config.pumpkin});
-        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'bee', value: config.bee});
+        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'bearbird', value: config.bear_bird });
+        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'termite', value: config.termite });
+        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'crocodile', value: config.crocodile });
+        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'walrus', value: config.walrus });
+        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'pumpkin', value: config.pumpkin });
+        this.ModLoader.gui.tunnel.send('BkCharacter:set', { form: 'bee', value: config.bee });
     }
 
     @TunnelMessageHandler('BkCharacter:update')
@@ -83,7 +85,7 @@ export class BkCharacter implements IPlugin {
                 this.core.character.bee_id = (<any>API.CharacterType)[evt.value];
                 break;
         }
-        
+
         // Save config
         this.ModLoader.config.save();
     }
